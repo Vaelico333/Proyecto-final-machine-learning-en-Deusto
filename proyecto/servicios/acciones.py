c6c468de3c2a15e35b551944fc39c4fe6e0ee4b2
Darío <darzoga@gmail.com>
2025-12-29 11:46:32 +0100

    Update acciones.py
    Nuevos textos y función para extraer y enviar los datos de df.info()

--- proyecto/servicios/acciones.py
+++ proyecto/servicios/acciones.py
@@ -3,7 +3,7 @@ from servicios.analisis import *
 class textos:
     def bienvenida():
         """Devuelve el texto de la página de bienvenida de la aplicación."""
-        texto = '''    Bienvenido a este ejercicio de final de curso de Machine Learning con Deusto, realizado por Darío Zoreda.
+        texto = u'''    Bienvenido a este ejercicio de final de curso de Machine Learning con Deusto, realizado por Darío Zoreda.
 El objetivo es crear y analizar un conjunto de datos ficticios relativos a la salud de un número de personas. 
 
 De manera aleatoria, crearemos un número n de pacientes ficticios, con un ID, y datos de edad, peso, altura, presión arterial y glucosa.
@@ -27,9 +27,10 @@ Finalmente, veamos unas gráficas demostrativas del rendimiento del modelo:
 - Ganancias y pérdidas
 '''
         return texto
+    
     def creacion():
         """Devuelve el texto de explicación de la creación de los datos."""
-        texto = '''En este paso, crearemos la base de datos.
+        texto = u'''En este paso, crearemos la base de datos.
 Primero, estableceré unos rangos de peligro basados en lo que la ciencia médica dice, para el IMC, la presión arterial y la glucosa, y todo ello relacionado con unos rangos de edad.
 Para agregar complejidad a los datos, creé las columnas de altura, peso y glucosa con dos tipos de unidad de medida cada una, distribuida de manera aleatoria.
 Para asegurar una distribución realista, utilizaré una distribución normal o de campana de Gauss, estableciendo una desviación estándar tal que produzca datos extremos más o menos realistas, que nos permitan crear esos datapoints en los que el paciente debe ser hospitalizado.
@@ -38,26 +39,31 @@ Una vez los datos están completos y coherentes, agregamos errores: de manera al
 Finalmente, guardamos los datos en un archivo de formato csv: datos_forjados.csv'''
         return texto
     
+    def transf_num():
+
+        texto = u'''Debido al formato en que hemos recibido los datos, hay varias columnas que son de tipo "object", y por tanto, incompatibles con un modelo de clasificación o regresión.
+Además, hay columnas con más de un tipo de unidad, lo cual es un gran problema, ya que al tener diferentes escalas, desvirtuarían mucho la capacidad predictiva del modelo.
+Por tanto, en el backend realizaremos dos operaciones fundamentales:
+- Quitar el nombre de la unidad, dejando sólo el número.
+- Convertir el número a una unidad estándar, y cambiar su tipo a float.
+
+Con esto, tendremos un DataFrame compuesto de 7 características, 6 de ellas numéricas y una categórica (hospitalización)
+
+'''
+        return texto
+    
+    def trat_err():
+        texto = ''' '''
+        return texto
+    
 class info:
-    def info_datos_originales(url: str = 'datos_forjados.csv'):
-        """
-        Abre los datos originales y 
-        devuelve un resumen de las características en texto
-        """
-        import io
-        import os
+    def extraer_info(info: list) -> list:
         import re
-        url = os.path.join(os.path.dirname(__file__), url)
-        df = leer_datos.abrir_csv(url)
-        buffer = io.StringIO()
-        df.info(buf=buffer, memory_usage=False)
-        lista_texto = buffer.getvalue().split('\n')
-        texto = '''Cantidad de datos no nulos y tipo de dato por columna:\n'''
         col_nom = []
         non_null = []
         dtype = []
         patron = r'\s+\d+\s+(\w+)\s+(\d+)\s+non-null\s+(\w+)'
-        for palabra in lista_texto:
+        for palabra in info:
             grupos = re.search(patron, palabra)
             if grupos:
                 re_col = grupos.group(1)
@@ -66,14 +72,45 @@ class info:
                 col_nom.append(re_col)
                 non_null.append(re_null)
                 dtype.append(re_dtype)
-                for n, col in enumerate(col_nom):
-                    texto_col = f'''Columna "{col}":
+            else:
+                continue
+        return col_nom, non_null, dtype
+    
+    def extraer_descripcion(df):
+        df.describe()
+
+    def info_datos_originales(url: str = 'datos_forjados.csv'):
+        """
+        Abre los datos originales y 
+        devuelve un resumen de las características en texto
+        """
+        import io
+        import os
+        # Leemos el archivo csv para crear un Dataframe
+        url = os.path.join(os.path.dirname(__file__), url)
+        df = leer_datos.abrir_csv(url)
+
+        # Creamos un buffer para poder recoger la información del método .info()
+        buffer = io.StringIO()
+        df.info(buf=buffer, memory_usage=False)
+
+        # Lo dividimos por líneas
+        lista_texto = buffer.getvalue().split('\n')
+        
+        # Extraemos las listas de datos que necesitamos y se los aplicamos a un texto
+        col_nom, non_null, dtype = info.extraer_info(lista_texto)
+        texto = '''Cantidad de datos no nulos y tipo de dato por columna:\n'''
+        for n, col in enumerate(col_nom):
+            texto_col = f'''Columna "{col}":
 - Datos no nulos: {non_null[n]}
 - Datos nulos: {len(df) - int(non_null[n])}
 - Tipo de dato: {dtype[n]}
 ********************************
 '''
-            else:
-                continue
+        # Unimos los textos y los devolvemos 
             texto += texto_col
         return texto
+    
+    def info_datos_num():
+        
+        pass

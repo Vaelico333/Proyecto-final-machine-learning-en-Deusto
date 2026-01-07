725db4df263ec712735d77f7cf1b54b7f826d290
Darío <darzoga@gmail.com>
2026-01-07 15:44:13 +0100

    Create graficos.py
    Nuevo módulo

---
+++ proyecto/servicios/graficos.py
@@ -0,0 +1,66 @@
+from servicios.analisis import *
+class Eda:
+
+    def col_hosp(figura):
+        col = 'hospitalizacion'
+        # Recuperamos los datos
+        df_col = Analisis.cadena_a_numero()
+        df_col = df_col['hospitalizacion']
+
+        # Creamos un subplot
+        ax = figura.add_subplot(111)
+
+        # Contamos las ocurrencias de cada valor
+        conteo = df_col.value_counts()
+
+        # Creamos la gráfica de barras
+        ax.bar(conteo.index, conteo.values)
+
+        # Nombramos los ejes
+        ax.set_xlabel(col)
+        ax.set_ylabel('Entradas')
+        ax.set_title('Distribución de los datos')
+        return ax
+    
+    def cols_num(figura, col):
+
+        import pandas as pd
+        # Recuperamos los datos
+        df_col = Analisis.cadena_a_numero()
+        df_col, datos = Analisis.limpiar_errores(df_col, col, 'columna')
+
+        # Convertimos df_col a dataframe y lo ordenamos por valores   
+        df_col = pd.DataFrame(df_col)
+        df_col = df_col.sort_values(col)
+        df_col = df_col.reset_index(drop=True)
+
+        # Recibimos los datos de la columna indicada
+        y = df_col
+
+        # Creamos un subplot
+        ax = figura.add_subplot(111)
+                        
+        # Creamos la gráfica de línea
+        ax.plot(y)
+
+        #Nombramos los ejes
+        ax.set_xlabel('Entradas')
+        ax.set_ylabel(col)
+        ax.set_title('Distribución de los datos')
+        return ax
+    
+class GrafModelo:
+    def graf_reglog():
+        pass
+
+    def graf_bosque():
+        pass
+
+    def graf_xgb():
+        pass
+
+class Evaluación:
+    pass
+
+class Informes:
+    pass

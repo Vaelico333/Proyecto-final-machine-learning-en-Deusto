c3f51679331a1fb04fa3dd33214e77ea5166e7f5
Darío <darzoga@gmail.com>
2026-01-10 09:10:25 +0100

    Update trabajador.py
    Creado Decorador para gestionar barra de progreso

--- proyecto/servicios/trabajador.py
+++ proyecto/servicios/trabajador.py
@@ -1,8 +1,13 @@
 from PyQt5.QtCore import QRunnable, pyqtSignal, QObject, pyqtSlot
 
+class ControlEntrenamiento:
+    def __init__(self, iteraciones_iniciales=100):
+        self.total_pasos = iteraciones_iniciales
+
 class SeñalesTrabajador(QObject):
     terminado = pyqtSignal(object)
     error = pyqtSignal(str)
+    progreso = pyqtSignal(int)
 
 class Trabajador(QRunnable):
     def __init__(self, funcion, *args, **kwargs):
@@ -15,7 +20,29 @@ class Trabajador(QRunnable):
     @pyqtSlot()
     def run(self):
         try:
+            self.kwargs['señal_progreso'] = self.señales.progreso.emit
             resultado = self.funcion(*self.args, **self.kwargs)
             self.señales.terminado.emit(resultado)
+
         except Exception as e:
+            import traceback
+            traceback.print_exc() 
             self.señales.error.emit(str(e))
+
+class Decorador():
+
+    def progreso(func):
+        import functools
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            llamada = kwargs.get('señal_progreso')
+            control = kwargs.get('objeto_control')
+
+            def reporte(paso_actual):
+                if llamada and control:
+                    porcentaje = int((paso_actual / control.total_pasos) * 100)
+                    llamada(min(porcentaje, 100))
+
+            kwargs['reporte_progreso'] = reporte
+            return func(*args, **kwargs)
+        return wrapper

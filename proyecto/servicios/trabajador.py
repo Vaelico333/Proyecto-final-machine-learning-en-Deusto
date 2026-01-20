023f0646d4101f3117def92f6cceda0bdbb57b06
Darío <darzoga@gmail.com>
2026-01-20 21:04:06 +0100

    Update trabajador.py
    Nueva clase CapturadorConsola que recoge la salida de verbose de gscv por stdout y la utiliza para contabilizar el progreso.
    También añadida lógica a Trabajador para que incorpore la lógica nueva sólo en caso de recibir el capturador

--- proyecto/servicios/trabajador.py
+++ proyecto/servicios/trabajador.py
@@ -1,4 +1,7 @@
-from PyQt5.QtCore import QRunnable, pyqtSignal, QObject, pyqtSlot
+import sys
+import re
+import traceback
+from PyQt5.QtCore import QObject, pyqtSignal, QRunnable, pyqtSlot
 
 class ControlEntrenamiento:
     def __init__(self, iteraciones_iniciales=100):
@@ -9,25 +12,104 @@ class SeñalesTrabajador(QObject):
     error = pyqtSignal(str)
     progreso = pyqtSignal(int)
 
+class CapturadorConsola(QObject):
+    """Procesa el verbose de Scikit-learn para extraer progreso."""
+    progreso_total = pyqtSignal(int)   # Para Barra Global (iteraciones)
+    progreso_parcial = pyqtSignal(int)  # Para Barra Parcial (fits/candidatos)
+    mensaje_status = pyqtSignal(dict)    # Para QLabel informativo
+
+    def __init__(self):
+        super().__init__()
+        self.total_etapas = 1
+        self.fits_totales_etapa = 0
+        self.fits_actuales_etapa = 0
+        self.etapa_actual = 0
+        self.candidatos = 0
+        self.recuento_cv = 1
+
+    def write(self, text):
+        # Si no hay texto o es todo espacios, terminamos la función de inmediato
+        if not text or text.isspace(): return
+
+        # Extraemos el número de iteraciones totales
+        if 'n_iterations:' in text:
+            m = re.search(r'n_iterations: (\d+)', text)
+            if m:
+                max_iter = int(m.group(1))
+                self.total_etapas = max_iter
+
+        # Extraemos el número de iteración actual y se lo enviamos a la barra de progreso total
+        if "iter:" in text:
+            m = re.search(r'iter: (\d+)', text)
+            if m:
+                self.recuento_cv = 0
+                # +1 porque las iteraciones empiezan en 0
+                self.etapa_actual = int(m.group(1)) + 1
+                prog_t = int((self.etapa_actual / self.total_etapas) * 100)
+                self.progreso_total.emit(min(prog_t, 100))
+        
+        # Contamos el número de candidatos totales y los fits, que serán el número de pasos de nuestra barra parcial
+        if "totalling" in text and "fits" in text:
+            m = re.search(r'Fitting (\d+) folds for each of (\d+) candidates, totalling (\d+) fits', text)
+            if m:
+                self.candidatos = int(m.group(2))
+                entrenamientos = int(m.group(3))
+                self.fits_totales_etapa = entrenamientos
+                self.fits_actuales_etapa = 0
+                self.progreso_parcial.emit(0)
+
+        # Contamos cuántos candidatos se han analizado (cambiar si se cambian los "folds")
+        if "[CV 3/3" in text:
+            self.recuento_cv += 1
+
+        # Contamos cada fit finalizado
+        conteo_fits = text.count("[CV")
+        if conteo_fits > 0:
+            self.fits_actuales_etapa += conteo_fits
+            if self.fits_totales_etapa > 0:
+                prog_p = int((self.fits_actuales_etapa / self.fits_totales_etapa) * 100)
+                self.progreso_parcial.emit(min(prog_p, 100))
+        
+        mensaje = {}
+        mensaje['iter'] = self.etapa_actual
+        mensaje['cand'] = self.recuento_cv
+        mensaje['cand_total'] = self.candidatos
+        self.mensaje_status.emit(mensaje)
+    
+    def flush(self): pass
+
 class Trabajador(QRunnable):
-    def __init__(self, funcion, *args, **kwargs):
+    def __init__(self, funcion, *args, capturador=None, **kwargs):
         super().__init__()
         self.funcion = funcion
         self.args = args
         self.kwargs = kwargs
+        self.capturador = capturador        
         self.señales = SeñalesTrabajador()
 
     @pyqtSlot()
     def run(self):
-        try:
-            self.kwargs['señal_progreso'] = self.señales.progreso.emit
-            resultado = self.funcion(*self.args, **self.kwargs)
-            self.señales.terminado.emit(resultado)
+        stdout_original = sys.stdout
+        if self.capturador:
+            sys.stdout = self.capturador 
+            try:
+                resultado = self.funcion(*self.args, **self.kwargs)
+                self.señales.terminado.emit(resultado)
+            except Exception as e:
+                self.señales.error.emit(str(e))
+                traceback.print_exc()
+            finally:
+                sys.stdout = stdout_original
+        else:
+            try:
+                self.kwargs['señal_progreso'] = self.señales.progreso.emit
+                resultado = self.funcion(*self.args, **self.kwargs)
+                self.señales.terminado.emit(resultado)
 
-        except Exception as e:
-            import traceback
-            traceback.print_exc() 
-            self.señales.error.emit(str(e))
+            except Exception as e:
+                import traceback
+                traceback.print_exc() 
+                self.señales.error.emit(str(e))
 
 class Decorador():
 

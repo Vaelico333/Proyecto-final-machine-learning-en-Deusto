9c4c204eac6afa0a00c7d16c9f4247cdb21553ff
Darío <darzoga@gmail.com>
2026-01-25 08:40:10 +0100

    Update analisis.py
    Nuevos comentarios.
    Creada lógica de evaluación de los modelos.

--- proyecto/servicios/analisis.py
+++ proyecto/servicios/analisis.py
@@ -40,7 +40,8 @@ class Leer_Datos:
         return df_muestra
     
 class Analisis():
-    import pandas as pd
+    from pandas import DataFrame
+    from numpy import ndarray
     def operacion_str(num: float, *args) -> float:
         """
         Ejecuta la operación u operaciones contenidas en args sobre num.
@@ -70,7 +71,7 @@ class Analisis():
             num = operadores[operacion](num, float(numero))
         return num
 
-    def cadena_a_numero(df: pd.DataFrame = pd.DataFrame(), cols: list = None, modo: str = 'num') -> pd.DataFrame:
+    def cadena_a_numero(df: DataFrame = DataFrame(), cols: list = None, modo: str = 'num') -> DataFrame:
         """
         Convierte las columnas con medidas de cadena de caracteres a número de coma flotante, 
         elimina el nombre de la unidad y 
@@ -122,17 +123,19 @@ class Analisis():
             
             # Nos aseguramos de convertir todo a cadena de caracteres para 
             # evitar problemas con NaN y 0 introducidos como errores
-            df[columnas[col]] = df[col].astype(str)
+            df[columnas[col]] = df[col] #.astype(str)
             
             # Esta lista contendrá la columna ya convertida en numérica
             valores_finales = []
             # Presión arterial tiene un formato especial, y requiere otro tratamiento
             if col == 'presion_arterial':
                 for valor in df[columnas[col]]:
+                    valor = str(valor)
                     valor_limpio = valor.split('/')[0] # Cogemos el número que va antes de la barra, ya que es la presión sistólica y es más representativo
                     valores_finales.append(valor_limpio)
             else:
                 for valor in df[columnas[col]]: # Iteramos por la columna
+                        valor = str(valor)
                         agregado = False # Creamos una "flag" que nos indique si ya se ha agregado un dato
                         if not agregado:
                             for unidad in unidades_estandar: # Si la unidad es estándar, le quitamos la unidad, lo convertimos en float y lo agregamos directamente
@@ -179,7 +182,7 @@ class Analisis():
                             
                   
     
-    def limpiar_errores(df: pd.DataFrame = pd.DataFrame(), cols: str = None, modo: str = 'total') -> pd.DataFrame | tuple[pd.DataFrame, tuple[int, int, int]]:
+    def limpiar_errores(df: DataFrame = DataFrame(), cols: str = None, modo: str = 'total') -> DataFrame | tuple[DataFrame, tuple[int, int, int]]:
         """
         Docstring for limpiar_errores
         
@@ -205,10 +208,10 @@ class Analisis():
             return df
         
         if modo == 'columna' and cols:
-            num_no_nan = df.count()
-            num_nan = len(df) - num_no_nan
-            num_cero = len(df[df[cols] == 0])
-            num_neg = len(df[df[cols] < 0])
+            num_no_nan = df.count() # Contamos los valores no nulos
+            num_nan = len(df) - num_no_nan # Restamos para extraer los valores nulos
+            num_cero = len(df[df[cols] == 0]) # Filtramos para encontrar el número de datos con valor 0
+            num_neg = len(df[df[cols] < 0]) # Filtramos para sacar los valores negativos
 
             df[cols] = df[cols].fillna(value=df[cols].mean()) # Reemplazamos NaN por la media de valores
             df[cols] = df[cols].replace(to_replace=0, value=df[cols].mean()) # Reemplazamos 0 por la media de valores
@@ -216,3 +219,89 @@ class Analisis():
             df.loc[df[cols] < 0, cols] *= -1 # Si el dato es negativo, lo volvemos positivo
             return df[cols], (num_nan[cols], num_cero, num_neg)
 
+    def log_loss_modelo(modelo_dict: dict) -> list[float]:
+        """
+        Docstring for log_loss_modelo
+        
+        :param modelo_dict: Description
+        :type modelo_dict: dict
+        :return: Description
+        :rtype: list[float]
+        """
+        from sklearn.metrics import log_loss
+        from sklearn.preprocessing import label_binarize
+        import numpy as np
+
+        X_test = modelo_dict['X_test']
+        y_test = modelo_dict['y_test']
+        modelo = modelo_dict['modelo']
+        y_pred_proba = modelo.predict_proba(X_test)
+        n_clases = y_pred_proba.shape[1]
+
+        y_test_bin = label_binarize(y_test, classes=range(n_clases))
+        if n_clases == 2 and y_test_bin.shape[1] == 1:
+            y_test_bin = np.hstack((1 - y_test_bin, y_test_bin))
+
+        log_losses = []
+        for i in range(n_clases):
+            ll = log_loss(y_test_bin[:, i], y_pred_proba[:, i], labels=[0, 1])
+            log_losses.append(ll)
+
+        return log_losses
+    
+    def confusion_matrix_modelo(modelo_dict: dict) -> ndarray:
+        """
+        Docstring for confusion_matrix_modelo
+        
+        :param modelo_dict: Description
+        :type modelo_dict: dict
+        :return: Description
+        :rtype: ndarray
+        """
+        from sklearn.metrics import confusion_matrix
+
+        y_test = modelo_dict['y_test']
+        y_pred = modelo_dict['y_pred']
+        cm =  confusion_matrix(y_test, y_pred)
+
+        return cm
+    
+    def roc_auc_modelo(modelo_dict: dict) -> list[ndarray, ndarray, float]:
+        """
+        Docstring for roc_auc_modelo
+        
+        :param modelo_dict: Description
+        :type modelo_dict: dict
+        :return: Description
+        :rtype: list[ndarray]
+        """
+        from sklearn.metrics import roc_curve, auc
+        X_test = modelo_dict['X_test']
+        y_test = modelo_dict['y_test']
+        modelo = modelo_dict['modelo']
+        y_pred_proba = modelo.predict_proba(X_test)
+        fpr, tpr, thresholds = roc_curve(y_test, y_pred_proba[:, 1])
+        roc_auc_curva = auc(fpr, tpr)
+
+        return [fpr, tpr, roc_auc_curva]
+
+    def importancia_caracteristicas_modelo(modelo_dict: dict) -> DataFrame:
+        """
+        Docstring for importancia_caracteristicas_modelo
+        
+        :param modelo_dict: Description
+        :type modelo_dict: dict
+        :return: Description
+        :rtype: DataFrame
+        """
+        import pandas as pd
+        X_test = modelo_dict['X_test']
+        modelo = modelo_dict['modelo']
+        características = X_test.columns.tolist()
+        importancias = modelo.feature_importances_
+
+        import_carac_df = pd.DataFrame({
+            'características': características,
+            'importancia': importancias}).sort_values('importancia', ascending=False)
+
+        return import_carac_df
